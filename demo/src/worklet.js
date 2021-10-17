a4734c4581270ff9798af93b5e4ed8f91abd5e7b
Amir Asghary <itsamirasghary@gmail.com>
2021-10-18 02:24:25 +0330

    installed husky and lint-staged

--- demo/src/worklet.js
+++ demo/src/worklet.js
@@ -1,68 +1,70 @@
 class Random {
-  static rangeRandom (range) {
-    return Math.random() * range;
-  }
+	static rangeRandom (range) {
+		return Math.random() * range;
+	}
 
-  static wholeRandom (range) {
-    return Math.floor(Random.rangeRandom(range));
-  }
+	static wholeRandom (range) {
+		return Math.floor(Random.rangeRandom(range));
+	}
 
-  static randSignRandom (range, bias) {
-    if (bias)
-      return Random.rangeRandom(range * 2) - range + bias;
+	static randSignRandom (range, bias) {
+		if (bias)
+			return Random.rangeRandom(range * 2) - range + bias;
 
-    return Random.rangeRandom(range * 2) - range;
-  }
+		return Random.rangeRandom(range * 2) - range;
+	}
 
-  static random () {
-    return Math.random();
-  }
+	static random () {
+		return Math.random();
+	}
 }
 
 function randomColor () {
-  return `rgb(
+	return `rgb(
     ${Random.wholeRandom(255)},
     ${Random.wholeRandom(255)},
     ${Random.wholeRandom(255)}
-  )`
+  )`;
 }
 
 class CheckerPaint {
-  paint(ctx, geom, styleMap) {
-    const amount = 100;
-    const respectBorders = true;
-    const padding = 0;
-    const [baseWidth, baseHeight] = [15, 35];
+	paint(ctx, geom, styleMap) {
+		console.log(styleMap);
+		const amount = 100;
+		const respectBorders = true;
+		const padding = 0;
+		const [baseWidth, baseHeight] = [15, 35];
 
-    for (let i = 0; i < amount; i++) {
-      const color = randomColor();
-      const scale = 1 + Random.randSignRandom(0.5);
-      const [width, height] = [baseWidth * scale, baseHeight * scale];
-      let [x, y] = [ 
-        Random.rangeRandom(geom.width),
-        Random.rangeRandom(geom.height)
-      ];
-      const angle = Random.randSignRandom(3) * Math.PI / 180;
+		for (let i = 0; i < amount; i++) {
+			const color = randomColor();
+			const scale = 1 + Random.randSignRandom(0.5);
+			const [width, height] = [baseWidth * scale, baseHeight * scale];
+			let [x, y] = [ 
+				Random.rangeRandom(geom.width),
+				Random.rangeRandom(geom.height)
+			];
+			const angle = Random.randSignRandom(3) * Math.PI / 180;
 
-      if(respectBorders) {
-        x = Math.min(geom.width - width - padding, x);
+			if(respectBorders) {
+				x = Math.min(geom.width - width - padding, x);
 
-        y = Math.min(geom.height - height - padding, y);
-      }
+				y = Math.min(geom.height - height - padding, y);
+			}
       
-      ctx.beginPath();
+			ctx.beginPath();
 
-      ctx.translate((x + width)/2, (y + height)/2);
-      ctx.rotate(angle);
-      ctx.translate((x + width)/2 * -1, (y + height)/2 * -1);
+			ctx.translate((x + width)/2, (y + height)/2);
+			ctx.rotate(angle);
+			ctx.translate((x + width)/2 * -1, (y + height)/2 * -1);
 
-      ctx.fillStyle = color;
-      ctx.rect(x, y, width, height);
-      ctx.fill();
+			ctx.fillStyle = color;
+			ctx.rect(x, y, width, height);
+			ctx.fill();
 
-      ctx.rotate(-angle);
-    }
-  }
+			ctx.rotate(-angle);
+		}
+	}
 }
 
-registerPaint("confetti", CheckerPaint);
+// eslint-disable-next-line no-undef
+registerPaint('confetti', CheckerPaint);
